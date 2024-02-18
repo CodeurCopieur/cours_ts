1deb35499782580ebd35d44c15e3ff92dbd9cdef
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-02-18 15:07:03 +0100

    Les types de base

---
+++ Contenu/I Les Bases de TS/2.Les Types de bases/script.js
@@ -0,0 +1,12 @@
+"use strict";
+var str = 'hello';
+var num = 5;
+var tab = ['azer', 'aqwx'];
+var obj = { a: 'a', b: ['ddsd', 'dfdf'] };
+var toggle = true;
+var anything = { a: true, b: 'fdfd' };
+var randomNumber;
+var conversion = function (cels) {
+    return (cels * 9 / 5) + 32;
+};
+console.log(conversion(30));
---
+++ Contenu/I Les Bases de TS/2.Les Types de bases/script.ts
@@ -0,0 +1,15 @@
+let str: string = 'hello'
+let num:number = 5
+let tab: string[] = ['azer', 'aqwx']
+let obj: object = {a: 'a', b: ['ddsd', 'dfdf']}
+
+let toggle: boolean = true
+
+let anything: any = {a: true, b: 'fdfd'};
+let randomNumber : number;
+
+const conversion = (cels:number ) => {
+  return (cels * 9/5) + 32
+}
+
+console.log(conversion(30));
